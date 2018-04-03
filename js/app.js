1bc90a08a95de789b5163ba85296a3342cfece23
Otis <otisvickersgraver@gmail.com>
2018-04-03 10:41:48 +0100

    intro and project links added

--- js/app.js
+++ js/app.js
@@ -1,2 +1,8 @@
 console.log('JS Loaded?');
 console.log('Yes it is');
+
+$(document).ready(function() {
+  $('#fullpage').fullpage({
+    anchors: ['firstPage', 'secondPage', 'thirdPage']
+  });
+});
