fb1b043a52de39834c21437d392a6141b7f7ce52
Otis <otisvickersgraver@gmail.com>
2018-04-16 18:37:40 +0100

    added all project descriptions, next stage is devicons

--- js/app.js
+++ js/app.js
@@ -1,8 +1,2 @@
 console.log('JS Loaded?');
 console.log('Yes it is');
-
-$(document).ready(function() {
-  $('#fullpage').fullpage({
-    anchors: ['firstPage', 'secondPage', 'thirdPage']
-  });
-});
